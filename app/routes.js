3c9e46a18100265442d9b86750e4d87e22382674
Amir Liberzon <amir.lib@gmail.com>
2018-04-01 09:34:58 +0200

    Create  first version of Chat

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,16 @@
-var    express = require('express');
-var       path = require('path');
-var      mysql = require('mysql');
-var connection = mysql.createConnection({
+var express = require('express');
+var    path = require('path');
+var   mysql = require('mysql');
+var  moment = require('moment');
+var  config = {
     host     : 'localhost',
     user     : 'root',
     password : '',
     database : 'forchat'
-});
+};
 
 //create router obj
-var  router = express.Router();
+var router = express.Router();
 
 //export router
 module.exports = router;
@@ -17,30 +18,63 @@ module.exports = router;
 // route to homepage
 router.get('/', function (req, res) {
     res.render('pages/index');
+    /*let con = mysql.createConnection(config);
+    let sql ="SELECT * FROM chat";
+    con.query(sql, function (err, result, fields) {
+        if (err) {
+            return console.error(err.message);
+        }
+        res.header("Content-Type",'application/json');
+        res.send(JSON.stringify(result, null, 4));
+    });
+    con.end();*/
+    
 });
 
+/*router.post('/sendChat', function(req, res) {
+    let con = mysql.createConnection(config);
+    let sql ="INSERT INTO chat (text, user, date) VALUES (?,?,?)";
+    let timeStamp = moment().format("YYYY-MM-DD h:mm:ss");   
+    let todo = [req.body.chat, req.body.user, timeStamp];
+    con.query(sql, todo, function (err, results, fields) {
+        if (err) {
+            return console.error(err.message);
+        }
+    });
+    con.end();
+    res.redirect('back');
+});*/
+
 //route to register page
-router.get('/register', function (req, res) {
-    res.render('pages/register');
+/* router.get('/register', function (req, res) {
+    
 });
 
 router.post('/register', function(req, res) {
-    res.send('Thanks for contacting us, ' + req.body.user + '! We will respond shortly!');
-    connection.connect(function(err){
+    mysql.connect(function(err){
         if(!err) {
-            console.log("Database is connected ...");    
+            printItBitch("Database is connected ...");   
         } else {
-            console.log("Error connecting database ...  ");    
+            printItBitch("Error connecting database ...  ");
         }
-        /* var sql = "INSERT INTO users (user, pass) VALUES ('Amir', '22')";
-        connection.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("1 record inserted");
-        }); */
+        var sql = "SELECT user FROM users WHERE user = ?";
+        mysql.query(sql, [req.body.user], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            if (result.length > 0) {
+                mysql.end();
+                printItBitch("User is already in use");
+            } else {
+                sql = "INSERT INTO users (user, pass, email) VALUES ( '" + req.body.user + "', '" + req.body.pass1 + "', '" + req.body.email + "')";
+                mysql.query(sql, function (err, result) {
+                    mysql.end();
+                    if (err) {
+                        throw err;
+                    }
+                    printItBitch("1 record inserte");
+                });
+            }
+        });
     });
-});
-
-// route to login page
-router.get('/login', function (req, res) {
-    res.render('pages/login');
-});
+}); */
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var        express = require('express');
 var expressLayouts = require('express-ejs-layouts');
 var     bodyParser = require('body-parser');
 var            app = express();
+var           http = require('http').Server(app);
+var             io = require('socket.io')(http);
 var           port = 8080;
 
 // use ejs and express layouts
@@ -19,12 +21,21 @@ app.use('/', router);
 //set css, js files
 app.use(express.static(__dirname + '/public'));
 
+/*************** io SECTTION ***************/
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg) {
+      console.log('message: ' + msg);
+      io.emit('chat message', msg);
+    });
+  });
+
+/*********** END fot io SECTTION ***********/
 //route to 404 page
 app.use(function (req, res) {
     res.render('pages/404');
 });
 
 // start the server
-app.listen(port, function() {
+http.listen(port, function() {
     console.log('app started');
 });
