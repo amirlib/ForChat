a1eef0e31c41cd4f4e77308fd366f00a6b7bd41d
Amir Liberzon <amir.lib@gmail.com>
2020-10-13 11:34:05 +0300

    supporting environment variables

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-// server.js
-var        express = require('express');
+var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
-var            app = express();
-var           http = require('http').Server(app);
-var             io = require('socket.io')(http);
-var           port = 8080;
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const port = process.env.PORT;
 
 // use ejs and express layouts
 app.set('view engine', 'ejs');
-app.set('views','./public');
+app.set('views', './public');
 app.use(expressLayouts);
 
 //route our app
@@ -19,12 +19,12 @@ app.use('/', router);
 app.use(express.static(__dirname + '/public'));
 
 /*************** io SECTTION ***************/
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg, userName) {
-      console.log('user: ' + userName + ', message: ' + msg);
-      io.emit('chat message', msg, userName);
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg, userName) {
+        console.log('user: ' + userName + ', message: ' + msg);
+        io.emit('chat message', msg, userName);
     });
-  });
+});
 
 /*********** END fot io SECTTION ***********/
 //route to 404 page
@@ -32,7 +32,6 @@ app.use(function (req, res) {
     res.render('pages/404');
 });
 
-// start the server
-http.listen(process.env.PORT || port, function() {
-    console.log('app started');
+http.listen(port, () => {
+    console.log(`Server is up on port: ${port}.`);
 });
